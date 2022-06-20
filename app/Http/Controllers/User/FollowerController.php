666c76942c704219099a46fa5cfddbefbd80acab
Rauf Abbas <rafo.abbas@gmail.com>
2022-06-20 16:56:12 +0400

    add

--- app/Http/Controllers/User/FollowerController.php
+++ app/Http/Controllers/User/FollowerController.php
@@ -14,6 +14,31 @@ class FollowerController extends Controller
         return PendingFollowersResource::collection(auth()->user()->followers()->wherePivot('approved', false)->get());
     }
 
+    public function approve(User $user): \Illuminate\Http\Response
+    {
+        auth()->user()->approveRequest($user['id']);
+
+        return codioResponse([
+            'message' => 'Takib istəyin qəbul etdin '
+        ]);
+    }
+    public function reject(User $user): \Illuminate\Http\Response
+    {
+        auth()->user()->rejectRequest($user['id']);
+
+        return codioResponse([
+            'message' => 'Takib istəyin ləğv etdin '
+        ]);
+    }
+
+    public function cancel(User $user): \Illuminate\Http\Response
+    {
+        auth()->user()->cancelRequest($user['id']);
+
+        return codioResponse([
+            'message' => 'Takib istəyin ləğv etdin '
+        ]);
+    }
 
     public function follow(User $user): \Illuminate\Http\Response
     {
